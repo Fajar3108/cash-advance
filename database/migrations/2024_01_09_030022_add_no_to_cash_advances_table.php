5d7650d7f3b28cba63909db469d5347bc9467ef5
Fajar <maulanafajaribrahim@gmail.com>
2024-01-09 10:24:58 +0700

    feat(ca): add no field to ca table

---
+++ database/migrations/2024_01_09_030022_add_no_to_cash_advances_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cash_advances', function (Blueprint $table) {
+            DB::statement('ALTER TABLE cash_advances ADD no INT UNIQUE NOT NULL AUTO_INCREMENT AFTER id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cash_advances', function (Blueprint $table) {
+            $table->dropColumn('no');
+        });
+    }
+};
