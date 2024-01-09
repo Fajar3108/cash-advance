213561aad355c677a37c1f3704da8f26c92e93bd
Fajar <maulanafajaribrahim@gmail.com>
2024-01-09 10:56:34 +0700

    fix(ca): change lable for field name

--- resources/views/cash-advance/partials/form.blade.php
+++ resources/views/cash-advance/partials/form.blade.php
@@ -1,5 +1,5 @@
 <div class="mb-5">
-    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama CA</label>
     <input type="text" id="name" name="name"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" required
         value="{{ old('name') ?? $cashAdvance->name }}">
