36aa6ae391f86bdb178379c82d4931b35220b47a
Fajar <maulanafajaribrahim@gmail.com>
2024-01-09 10:55:37 +0700

    feat(profile): display user signature in profile

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -47,6 +47,10 @@
             <small class="text-red-600">{{ $message }}</small>
             @enderror
         </div>
+        <div>
+            <img src="{{ asset('storage/' . auth()->user()->signature) }}" alt="Signature"
+                class="w-24 h-24 object-contain">
+        </div>
         <div class="flex items-center gap-4 justify-end">
             <button type="submit"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
