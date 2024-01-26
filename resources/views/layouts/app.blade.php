92135796ab95d4346c17628e2dbc9254db987436
Fajar <maulanafajaribrahim@gmail.com>
2024-01-26 16:21:40 +0700

    feat: add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }}</title>
+    <link rel="icon" href="{{ asset('images/logo_hma.png') }}" type="image/x-icon">
     @vite('resources/css/app.css')
 </head>
 
