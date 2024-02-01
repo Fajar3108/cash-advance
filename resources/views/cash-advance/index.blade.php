61fb5d3291837e074c948d50499c43213b2f096e
Fajar <maulanafajaribrahim@gmail.com>
2024-02-01 09:31:17 +0700

    fix: add min height for table

--- resources/views/cash-advance/index.blade.php
+++ resources/views/cash-advance/index.blade.php
@@ -43,7 +43,7 @@
         </div>
     </div>
     @endif
-    <div class="overflow-x-auto">
+    <div class="overflow-x-auto min-h-60">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
