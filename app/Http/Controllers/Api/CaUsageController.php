99414a367cde584e1b9473959b55257472575f9a
Fajar <maulanafajaribrahim@gmail.com>
2024-02-02 13:28:25 +0700

    fix(ca): fix where condition

--- app/Http/Controllers/Api/CaUsageController.php
+++ app/Http/Controllers/Api/CaUsageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Constants\StatusConstant;
 use App\Http\Controllers\Controller;
 use App\Models\CaUsage;
 use App\Models\User;
@@ -10,7 +11,7 @@ class CaUsageController extends Controller
 {
     public function getByUserId(User $user)
     {
-        $caUsages = CaUsage::where('user_id', $user->id)->where('is_approved', '!=', 1)->get();
+        $caUsages = CaUsage::where('user_id', $user->id)->where('status', '!=', StatusConstant::APPROVED)->get();
         return response()->json([
             'message' => "Get user's ca usages",
             'data' => $caUsages->pluck('id'),
